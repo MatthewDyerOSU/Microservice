68c6cf3c5a387e9c30ce733f4c968243e13c6042
MattDyer87 <71666354+MattDyer87@users.noreply.github.com>
2022-10-31 15:39:53 -0700

    Created simple image serving microservice with flask

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import os
+from werkzeug.utils import secure_filename
+ 
+app = Flask(__name__)
+ 
+UPLOAD_FOLDER = 'static/uploads/'
+ 
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+     
+ 
+@app.route('/')
+def home():
+    return render_template('index.html')
+ 
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    image = request.files['file']
+    if image.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if image and allowed_file(image.filename):
+        filename = secure_filename(image.filename)
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Your image is uploaded!')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Only png, jpg, jpeg, gif, image types allowed!')
+        return redirect(request.url)
+ 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+ 
+if __name__ == "__main__":
+    app.run()
